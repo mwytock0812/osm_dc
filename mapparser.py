9abcc2f125d1ff730c59d75432ef740c651a13a5
mwytock0812 <michael.wytock@gmail.com>
2014-07-16 19:44:17 -0400

    Add docstring

--- mapparser.py
+++ mapparser.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-Your task is to use the iterative parsing to process the map file and
-find out not only what tags are there, but also how many, to get the
-feeling on how much of which data you can expect to have in the map.
-The output should be a dictionary with the tag name as the key
-and number of times this tag can be encountered in the map as value.
-
-Note that your code will be tested with a different data file than the 'example.osm'
-"""
 import xml.etree.ElementTree as ET
 import pprint
 
 def count_tags(filename):
+    """Return a dictionary counting number of top-level tags.
+
+    Dictionary Format:
+    {tag_name : count}
+    """
+    # Initialize ouput dict. Build and root tree.
     tags = {}
     tree = ET.parse(filename)
     root = tree.getroot()
+
+    # Count top-level tag appearances by line in root.
     for line in root:
         if line.tag in tags:
             count = tags[line.tag]
@@ -24,20 +23,11 @@ def count_tags(filename):
             tags[line.tag] = 1
     return tags
 
-def test():
-
-    tags = count_tags('example.osm')
+def print_tags(osmfile):
+    """Pretty prints dictionary of tag counts."""
+    tags = count_tags(osmfile)
     pprint.pprint(tags)
-    assert tags == {'bounds': 1,
-                     'member': 3,
-                     'nd': 4,
-                     'node': 20,
-                     'osm': 1,
-                     'relation': 1,
-                     'tag': 7,
-                     'way': 1}
-
-    
 
 if __name__ == "__main__":
-    test()
+    OSMFILE = "sampled_dc.osm"
+    print_tags(OSMFILE)
