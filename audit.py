ae2ca38e4dd03d700149829e0e966e6028624d14
mwytock0812 <michael.wytock@gmail.com>
2014-07-17 16:43:07 -0400

    Add regex.sub street correction

--- audit.py
+++ audit.py
@@ -1,52 +1,46 @@
-"""
-Your task in this exercise has two steps:
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-- audit the OSMFILE and change the variable 'mapping' to reflect the changes needed to fix 
-    the unexpected street types to the appropriate ones in the expected list.
-    You have to add mappings only for the actual problems you find in this OSMFILE,
-    not a generalized solution, since that may and will depend on the particular area you are auditing.
-- write the update_name function, to actually fix the street name.
-    The function takes a string with street name as an argument and should return the fixed name
-    We have provided a simple test so that you see what exactly is expected
-"""
 import xml.etree.cElementTree as ET
 from collections import defaultdict
 import re
 import pprint
 
-OSMFILE = "example.osm"
+# Working regex for street street_re = re.compile(r'\b([Ss]t)(\.|\b)')
 street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
 
+expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place",
+            "Square", "Lane", "Road", "Trail", "Parkway", "Commons"]
 
-expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road", 
-            "Trail", "Parkway", "Commons"]
-
-# UPDATE THIS VARIABLE
-mapping = { "St.": "Street",
-            "St": "Street",
-            "Ave": "Avenue",
-            "Rd.": "Road"
+mapping = { r'\b([Ss]t)(\.|\b)': "Street",
+            r'\b([Rr]d)(\.|\b)': "Road",
+            r'\b([Aa]ve)(\.|\b)': "Avenue",
+            r'\b([Dd]r)(\.|\b)': "Drive"
             }
 
-
 def audit_street_type(street_types, street_name):
+    """Add street_type to street_types dictionary if not in expected.
+
+    Format:
+    {street_type : set(street_name)}
+    """
     m = street_type_re.search(street_name)
     if m:
         street_type = m.group()
         if street_type not in expected:
             street_types[street_type].add(street_name)
 
-
 def is_street_name(elem):
+    """Return ET element if it is a street."""
     return (elem.attrib['k'] == "addr:street")
 
 
 def audit(osmfile):
+    """Return a list of street names"""
     osm_file = open(osmfile, "r")
     street_types = defaultdict(set)
     for event, elem in ET.iterparse(osm_file, events=("start",)):
-
-        if elem.tag == "node" or elem.tag == "way":
+        if elem.tag in ["node", "relation", "way"]:
             for tag in elem.iter("tag"):
                 if is_street_name(tag):
                     audit_street_type(street_types, tag.attrib['v'])
@@ -55,38 +49,25 @@ def audit(osmfile):
 
 
 def update_name(name, mapping):
-    '''
-    non_period = {}
+    """Return corrected street name from regex in mapping."""
     for key, value in mapping.iteritems():
-        if key[-1] != ".":
-            non_period[key] = value
-        else:
-            name = name.replace(key, value)
-            return name
-    
-    for key, value in non_period.iteritems():
-        name = name.replace(key, value)
-        return name
-    '''
-    for key, value in mapping.iteritems():
-        if key in name:
-            name = name.replace(key, value)
-            return name
+        if re.search(key, name):
+            new_name = re.sub(key, value, name)
+            return new_name
 
-def test():
-    st_types = audit(OSMFILE)
-    assert len(st_types) == 3
+def print_audit(osmfile):
+    """Pretty print the st_types dictionary.
+    Print name suggestions
+    """
+    st_types = audit(osmfile)
     pprint.pprint(dict(st_types))
 
     for st_type, ways in st_types.iteritems():
         for name in ways:
             better_name = update_name(name, mapping)
             print name, "=>", better_name
-            if name == "West Lexington St.":
-                assert better_name == "West Lexington Street"
-            if name == "Baldwin Rd.":
-                assert better_name == "Baldwin Road"
 
 
 if __name__ == '__main__':
-    test()
+    OSMFILE = "sampled_dc.osm"
+    print_audit(OSMFILE)
