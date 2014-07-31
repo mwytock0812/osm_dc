b72cca689eb3dfa49ad536ffd5d53bce01c728fa
mwytock0812 <michael.wytock@gmail.com>
2014-07-31 13:19:18 -0400

    Add docstrings

--- tags.py
+++ tags.py
@@ -3,30 +3,17 @@
 import xml.etree.ElementTree as ET
 import pprint
 import re
-"""
-Your task is to explore the data a bit more.
-Before you process the data and add it into MongoDB, you should
-check the "k" value for each "<tag>" and see if they can be valid keys in MongoDB,
-as well as see if there are any other potential problems.
-
-We have provided you with 3 regular expressions to check for certain patterns
-in the tags. As we saw in the quiz earlier, we would like to change the data model
-and expand the "addr:street" type of keys to a dictionary like this:
-{"address": {"street": "Some value"}}
-So, we have to see if we have such tags, and if we have any tags with problematic characters.
-Please complete the function 'key_type'.
-"""
-
 
 lower = re.compile(r'^([a-z]|_)*$')
 lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
 problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
 
-
 def key_type(element, keys):
+    """Update a dictionary counting the number of tag attributes
+    in an ET element that contain characters of interest.
+    """
     if element.tag == "tag":
         k = element.attrib['k']
-        # print k
         if len(lower_colon.findall(k)) > 0:
             keys['lower_colon'] += 1
         elif len(lower.findall(k)) > 0:
@@ -37,24 +24,14 @@ def key_type(element, keys):
             keys['other'] += 1
     return keys
 
-
-
 def process_map(filename):
+    """Return the keys dictionary after parsing the whole XML file."""
     keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
     for _, element in ET.iterparse(filename):
         keys = key_type(element, keys)
 
     return keys
 
-
-
-def test():
-    # You can use another testfile 'map.osm' to look at your solution
-    # Note that the assertions will be incorrect then.
-    keys = process_map('example.osm')
-    pprint.pprint(keys)
-    assert keys == {'lower': 5, 'lower_colon': 0, 'other': 2, 'problemchars': 0}
-
-
 if __name__ == "__main__":
-    test()
+    keys = process_map('dc.osm')
+    pprint.pprint(keys)
